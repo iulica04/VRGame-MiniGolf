f14288a5ae5a3805ae16f2eba8f2b334b67750a2
Vreme Iulia <ivreme12@gmail.com>
2024-10-15 23:10:15 +0300

    add

---
+++ Assets/Scripts/CountHits.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountHits : MonoBehaviour
+{
+    private int hits = 0;
+    public ScoreManager scoreManager; // Reference to the ScoreManager
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ensure the ScoreManager is linked
+        if (scoreManager == null)
+        {
+            scoreManager = FindObjectOfType<ScoreManager>();
+            if (scoreManager == null)
+            {
+                Debug.LogError("ScoreManager not found in the scene. Please ensure it is added.");
+            }
+
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "fairways")
+        {
+            // Crește numărul de lovituri
+            hits++;
+
+            // Afișează mesajul în consolă
+            Debug.Log("Mingea a fost lovită de crosă! Numar de lovituri: " + hits);
+
+            // Update the score using ScoreManager
+            scoreManager.UpdateScore();
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using TMPro; // Asigură-te că importi biblioteca TextMeshPro
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public TextMeshProUGUI scoreText; // Referință la TextMeshProUGUI
+
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    public void UpdateScore()
+    {
+        score++;
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
